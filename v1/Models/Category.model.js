8a6a7ffc1652ec6f487d908812af165d70e2185d
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-18 20:26:42 +0600

    category model add

--- v1/Models/Category.model.js
+++ v1/Models/Category.model.js
@@ -4,9 +4,23 @@ const { ObjectId } = mongoose.Schema.Types;
 
 
 const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        required: [true, 'Please Provide a Category Name']
+    },
+    description: String,
+    imageURL: {
+        type: String,
+        validate: [validator.isURL, "Please Provide a Valid URL"]
+    }
 
 
 
+}, {
+    timestamps: true
 })
 
 const Category = mongoose.model("Category", categorySchema);
