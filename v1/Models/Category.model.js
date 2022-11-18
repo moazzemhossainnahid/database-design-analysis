7ba8f01278a7977e70adebb59fa448a3480c7626
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-18 20:21:48 +0600

    brand model add

---
+++ v1/Models/Category.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const { ObjectId } = mongoose.Schema.Types;
+
+
+const categorySchema = mongoose.Schema({
+
+
+
+})
+
+const Category = mongoose.model("Category", categorySchema);
+
+exports = Category;
